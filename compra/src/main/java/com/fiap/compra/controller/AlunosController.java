3c9b5a66e49fb576166306a7183c7514521743b5
Carpedianico <carpedianico@yahoo.com.br>
2023-03-19 20:14:33 -0300

    Adição da descrição do Swagger

--- compra/src/main/java/com/fiap/compra/controller/AlunosController.java
+++ compra/src/main/java/com/fiap/compra/controller/AlunosController.java
@@ -33,7 +33,7 @@ public class AlunosController {
     @Operation(summary = "Retorna uma lista de pessoas", description  = "Esse método retorna uma lista de alunos: filtro realizado por página e quantidade de itens a serem retornados")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Lista de alunos encontrada com sucesso!"),
-            @ApiResponse(responseCode = "404", description = "Erro ao buscar alunos"),
+            @ApiResponse(responseCode = "404", description = "Erro ao buscar alunos."),
     })
     public ResponseEntity listAll(@RequestHeader("page") Integer page, @RequestHeader("size") Integer size) {
         try {
@@ -47,6 +47,11 @@ public class AlunosController {
 
     @GetMapping("{id}")
     @RequestMapping(value = "/filtrarALunoPorId/{id}", method = RequestMethod.GET, produces="application/json")
+    @Operation(summary = "Chama um unico aluno por id", description  = "Esse método retorna um unico aluno: filtro realizado por id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aluno encontrado com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Erro ao buscar aluno."),
+    })
     public ResponseEntity findById(
             @PathVariable long id) {
         try {
@@ -54,33 +59,48 @@ public class AlunosController {
             return ResponseEntity.status(HttpStatus.OK).body(aluno.get()._toDto());
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HTTPMessageResponse("Nenhum cliente com id={" + id + "} foi encontrado"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HTTPMessageResponse("Nenhum aluno com id={" + id + "} foi encontrado"));
         }
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "Atualiza o aluno.", description  = "Esse método atualiza o aluno.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Informação do aluno atualizada com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Aluno não encontrado."),
+    })
     public ResponseEntity update(@PathVariable Long id, @RequestBody AlunoDTO alunoDTO) {
         try {
             alunosService.update(id, alunoDTO);
             return ResponseEntity.status(HttpStatus.OK).body("Aluno atualizado com sucesso!");
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HTTPMessageResponse("Nenhum cliente com id={" + id + "} foi encontrado"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HTTPMessageResponse("Nenhum aluno com id={" + id + "} foi encontrado"));
         }
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Deleta o aluno", description  = "Esse método deleta o aluno do banco de dados de acordo com o id fornecido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aluno deletado com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Aluno não encontrado! Erro ao deletar aluno."),
+    })
     public ResponseEntity delete(@PathVariable Long id) {
         try {
             alunosService.delete(id);
             return ResponseEntity.status(HttpStatus.OK).body("Aluno deletado com sucesso!");
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HTTPMessageResponse("Nenhum cliente com id={" + id + "} foi encontrado"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HTTPMessageResponse("Nenhum aluno com id={" + id + "} foi encontrado"));
         }
     }
 
     @GetMapping("/{id}/extrato")
+    @Operation(summary = "Retorna o extrato de compras do cartão.", description  = "Esse método retorna o extrato do cartão filtrado pelo id do aluno.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Extrato gerado com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Erro ao gerar extrato."),
+    })
     public ResponseEntity<List<CompraAlunoDTO>> getExtrato(@PathVariable Long id) {
         List<CompraAluno> extrato = alunosService.getExtrato(id);
         List<CompraAlunoDTO> extratoDTO = extrato.stream().map(CompraAluno::_toDto).collect(Collectors.toList());
@@ -89,6 +109,11 @@ public class AlunosController {
 
 
     @PostMapping("{id}/comprar")
+    @Operation(summary = "Cadastra uma compra com cartão de crédito no banco.", description  = "Esse método verifica se o limite é maior ou igual o valor da compra e salva a compra no banco de dados com status de cancelado ou aprovado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Verificação de limite realizada!"),
+            @ApiResponse(responseCode = "404", description = "Erro ao processar a compra."),
+    })
     public ResponseEntity comprar(
             @PathVariable long id) {
         // QUAL O VALOR DA COMPRA??? BODY: { valor:
@@ -103,6 +128,10 @@ public class AlunosController {
     }
 
     @PostMapping("upload")
+    @Operation(summary = "Cadastro de aluno via arquivo .TXT", description  = "Esse método grava no banco de dados todos os alunos contidos no arquivo .TXT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de alunos gravada com sucesso!"),
+    })
     public ResponseEntity uploadAlunos(
             @ RequestParam MultipartFile arquivo) throws Exception {
         alunosService.cadastroInicial(arquivo);
